f25684e5d6ba5f512618d1f1b3d15c2a01b77dc8
cubee <cubeecb+github@gmail.com>
2026-06-19 16:37:01 +1000

    cleanup, add watch+toggles to cas

--- Cas/script.lua
+++ Cas/script.lua
@@ -1,10 +1,23 @@
--- Auto generated script file --
+-- CassetteFox Figura Script
+-- by cubee
+--[[
 
-require("physBoneAPI")
+This script does the following:
+- Turn off Ears when a hat is worn (Toggle in Action Menu)
+- Customise outfit (currently only watch)
+
+]]
+
+require("physBoneAPI") -- by ChloeSpacedOut
 
 -- init config
 local configFile = "CassetteFox"
 config:setName(configFile)
+
+-- interpret empty config to mean a fresh install
+-- config should never be saved outside of running actions,
+-- so this should ALWAYS return nil on remotes
+local freshInstall = next(config:load()) == nil
 
 -- hide vanilla model
 vanilla_model.PLAYER:setVisible(false)
@@ -14,10 +27,12 @@ models:setPrimaryRenderType("TRANSLUCENT_CULL")
 models:setSecondaryRenderType("EYES")
 
 -- get elements from model
-local ears = models.model.root.Head.Ears
+local earsModel = models.model.root.Head.Ears
+local watchModel = models.model.root.RightArm.Watch
 
 -- variables
 local hatDodge = true
+local showWatch = true
 
 --entity init event, used for when the avatar entity is loaded for the first time
 function events.entity_init()
@@ -29,9 +44,9 @@ function events.tick()
 
   if (hatDodge) then
     local headItem = player:getItem(6):getID()
-    ears:setVisible(not (headItem:find("helmet") or headItem:find("cap") or headItem:find("hat")))
+    earsModel:setVisible(not (headItem:find("helmet") or headItem:find("cap") or headItem:find("hat")))
   else
-    ears:setVisible(true)
+    earsModel:setVisible(true)
   end
 
 end
@@ -48,33 +63,58 @@ function events.skull_render(delta, context)
 
 end
 
--- local sounds
-function toggleSound()
-  sounds:playSound("entity.item.pickup", player:getPos(), 1, 2, false)
+-- functions
+function showState(state)
+  return state and "SHOWING" or "HIDING"
+end
+
+-- sounds
+function toggleSound(state)
+  if (not player:isLoaded()) then return end
+  sounds:playSound("entity.item.pickup", player:getPos(), 0.25, state and 1.5 or 0.5, false)
 end
 
 -- pings and keypresses
-function pings.actionSetHatDodge(_hatDodge)
-  hatDodge = _hatDodge
+function pings.actionSetHatDodge(state)
+  hatDodge = state
+  toggleSound(state)
+end
+
+function pings.actionSetWatch(state)
+  watchModel:setVisible(state)
+  toggleSound(state)
 end
 
 -- set up action menu
 local mainPage = action_wheel:newPage()
 action_wheel:setPage(mainPage)
 
-hatDodge = true
 local actionToggleHatDodge = mainPage:newAction()
   :title("Toggle Hat Dodge")
   :item("minecraft:iron_helmet")
-  :hoverColor(1, 0, 1)
-  :onLeftClick(function()
-    hatDodge = not hatDodge
-    pings.actionSetHatDodge(hatDodge)
+  :hoverColor(0.8, 0.8, 0.3)
+  :setOnToggle(function(state)
+    pings.actionSetHatDodge(state)
+    print(showState(not state) .. " ears when a hat is worn.")
+    config:save("hatDodge", state)
 
-    print("Now", hatDodge and "HIDING" or "SHOWING", "ears when a hat is worn.")
-    toggleSound()
-    config:save("hatDodge", hatDodge)
-  end)
+    hatDodge = state
+  end
+)
 
--- set pings from config
-pings.actionSetHatDodge(config:load("hatDodge"))
+local actionToggleWatch = mainPage:newAction()
+  :title("Toggle Watch")
+  :item("minecraft:clock")
+  :hoverColor(0.6, 0.9, 0.6)
+  :setOnToggle(function(state)
+    pings.actionSetWatch(state)
+    print("Watch " .. showState(state))
+    config:save("showWatch", state)
+  end
+)
+
+-- send pings from config
+if (not freshInstall) then
+  pings.actionSetHatDodge(config:load("hatDodge"))
+  pings.actionSetWatch(config:load("showWatch"))
+end
