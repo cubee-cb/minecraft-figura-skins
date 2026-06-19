51aace31dbf6405394bc9fa5bb5bfb52b1c76779
cubee <cubeecb+github@gmail.com>
2026-06-19 14:12:54 +1000

    adjust cas pivots to make armour work properly and add head parrot

--- Cas/script.lua
+++ Cas/script.lua
@@ -2,7 +2,11 @@
 
 require("physBoneAPI")
 
---hide vanilla model
+-- init config
+local configFile = "CassetteFox"
+config:setName(configFile)
+
+-- hide vanilla model
 vanilla_model.PLAYER:setVisible(false)
 
 -- BACKFACE CULLING WOOO
@@ -12,6 +16,9 @@ models:setSecondaryRenderType("EYES")
 -- get elements from model
 local ears = models.model.root.Head.Ears
 
+-- variables
+local hatDodge = true
+
 --entity init event, used for when the avatar entity is loaded for the first time
 function events.entity_init()
 
@@ -20,8 +27,12 @@ end
 --tick event, called 20 times per second
 function events.tick()
 
-  local headItem = player:getItem(6):getID()
-  ears:setVisible(not (headItem:find("helmet") or headItem:find("cap")))
+  if (hatDodge) then
+    local headItem = player:getItem(6):getID()
+    ears:setVisible(not (headItem:find("helmet") or headItem:find("cap") or headItem:find("hat")))
+  else
+    ears:setVisible(true)
+  end
 
 end
 
@@ -37,4 +48,33 @@ function events.skull_render(delta, context)
 
 end
 
+-- local sounds
+function toggleSound()
+  sounds:playSound("entity.item.pickup", player:getPos(), 1, 2, false)
+end
+
 -- pings and keypresses
+function pings.actionSetHatDodge(_hatDodge)
+  hatDodge = _hatDodge
+end
+
+-- set up action menu
+local mainPage = action_wheel:newPage()
+action_wheel:setPage(mainPage)
+
+hatDodge = true
+local actionToggleHatDodge = mainPage:newAction()
+  :title("Toggle Hat Dodge")
+  :item("minecraft:iron_helmet")
+  :hoverColor(1, 0, 1)
+  :onLeftClick(function()
+    hatDodge = not hatDodge
+    pings.actionSetHatDodge(hatDodge)
+
+    print("Now", hatDodge and "HIDING" or "SHOWING", "ears when a hat is worn.")
+    toggleSound()
+    config:save("hatDodge", hatDodge)
+  end)
+
+-- set pings from config
+pings.actionSetHatDodge(config:load("hatDodge"))
